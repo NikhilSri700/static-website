d6b0095075bab3d5e3dbaa1d77cdd5cfe9967a32
Nikhil Srivastav <44522355+NikhilSri700@users.noreply.github.com>
2022-04-04 00:08:26 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const thisForm = document.getElementById('myForm');
+thisForm.addEventListener('submit', async function (e) {
+    e.preventDefault();
+    const formData = new FormData(thisForm).entries()
+    const response = await fetch('https://cuv1je6ih0.execute-api.us-east-1.amazonaws.com/dev', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(Object.fromEntries(formData))
+    });
+
+    const result = await response.json();
+    console.log(result)
+});
